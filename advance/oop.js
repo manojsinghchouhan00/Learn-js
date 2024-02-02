0e1634f3748f3a30a502006d7a207727abbdf8a2
manojsinghchouhan00 <manojsinghchouhan00@gmail.com>
2024-02-02 12:56:58 +0530

    BOM  added

--- advance/oop.js
+++ advance/oop.js
@@ -1,19 +1,19 @@
 // Defining object :-----
 
 // let person = {
-// 	first_name: 'Mukul',
-// 	last_name: 'Latiyan',
+//     first_name: 'Mukul',
+//     last_name: 'Latiyan',
 
-// 	//method
-// 	getFunction: function () {
-// 		return (`The name of the person is
+//     //method
+//     getFunction: function () {
+//         return (`The name of the person is
 // 		${person.first_name} ${person.last_name}`)
-// 	},
-// 	//object within object
-// 	phone_number: {
-// 		mobile: '12345',
-// 		landline: '6789'
-// 	}
+//     },
+//     //object within object
+//     phone_number: {
+//         mobile: '12345',
+//         landline: '6789'
+//     }
 // }
 // console.log(person.getFunction());
 // console.log(person.phone_number.landline);
@@ -27,8 +27,8 @@
 // 	this.last_name = last_name;
 // }
 // // Creating new instances of person object
-// let person1 = new person('Mukul', 'Latiyan');
-// let person2 = new person('Rahul', 'Avasthi');
+// let person1 = new person('Mukul', 'Bhatia');
+// let person2 = new person('Rahul', 'Singh');
 
 // console.log(person1.first_name);
 // console.log(`${person2.first_name} ${person2.last_name}`);
@@ -40,11 +40,11 @@
 // Object.create() example a simple object with some properties
 
 // const coder = {
-// 	isStudying: false,
-// 	printIntroduction: function () {
-// 		console.log(`My name is ${this.name}. Am I
+//     isStudying: false,
+//     printIntroduction: function () {
+//         console.log(`My name is ${this.name}. Am I
 // 			studying?: ${this.isStudying}.`)
-// 	}
+//     }
 // }
 // // Object.create() method
 // const me = Object.create(coder);
@@ -56,7 +56,7 @@
 // me.isStudying = true;
 
 // me.printIntroduction();
-
+// console.log("me object :-",me)
 // -------------------------------------class-------------------------------------
 
 
---
+++ new.js
@@ -0,0 +1,36 @@
+
+let a = 50;
+
+{
+
+    let c, b;
+
+    b = 50
+
+    c = 10;
+
+    let d = b / c;
+
+    console.log("Sum of b+c :", a, c + b, d);
+
+    document.write("Sum of b+c :", c + b);
+}
+
+console.log(a);
+
+let firstName = "sapna"
+
+
+// pascal case convensition :-  PascalCaseConvention
+// camal case convensition :- camelCaseConvantion
+
+
+
+function myFun() {
+    let a = 22;
+}
+
+
+var d; // declayer  g  huist ha  support old
+d = 10; // assign   b       nhi           nhi
+d = 50;  // reAssign b      nhi           nhi
